c51d4225dd56eea9a0928b78c84e30744f8add7d
Deanna Brinkley <deannajbrinkley@icloud.com>
2019-05-22 18:45:32 -0700

    added homework 6

---
+++ Homework6/Homework6/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SendCard()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendCard(Models.BirthdayCard birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CardSent", birthdayCard);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
